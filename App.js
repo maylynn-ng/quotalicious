34f7f4dd76c6b4a242f8e3cdad067b615f7014ff
Maylynn <maylynn.ng94@gmail.com>
2020-07-29 18:17:56 +0100

    implement swipe for new quote and image separately

--- App.js
+++ App.js
@@ -2,24 +2,25 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, SafeAreaView, ImageBackground, FlatList } from 'react-native';
 import HomeQuote from './components/homeQuote';
 import { randomQuote, randomPicture } from './ApiClientService';
+import GestureRecognizer, { swipeDirections } from 'react-native-swipe-gestures';
 
 
 export default function App() {
-  const [quote, setQuote] = useState([{quote: "I'm a frickin quote", author: "me, bitch"}]);
-  const [picture, setPicture] = useState('https://i.picsum.photos/id/566/200/300.jpg?hmac=gDpaVMLNupk7AufUDLFHttohsJ9-C17P7L-QKsVgUQU');
+  const [quote, setQuote] = useState({});
+  const [picture, setPicture] = useState([]);
 
   const getRandomQuote = () => {
     randomQuote()
       .then(res => {
-        setQuote([{
+        setQuote({
           quote: res.quote.quoteText,
-          author: res.quote.quoteAuthor}])
+          author: res.quote.quoteAuthor})
       });
   }
 
   const getRandomPicture = () => {
     randomPicture()
-      .then(res => setPicture(res.url))
+      .then(res => setPicture([res.url]))
   }
 
   useEffect(() => {
@@ -29,36 +30,29 @@ export default function App() {
 
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.container}> 
+    <SafeAreaView>
+      <GestureRecognizer
+        onSwipeRight={() => getRandomPicture()}>
+      <View> 
         <ImageBackground 
           style={styles.picture}
-          source={{uri: picture}}>
-          <FlatList
-            data={quote}
-            keyExtractor={item => item.quote}
-            renderItem= {
-              ({item}) =>
-              <HomeQuote quote={item.quote} author={item.author}/>
-            }
-            onRefresh={() => {
-              //getRandomPicture()
-              getRandomQuote()
-            }}
-            refreshing={false}/>
+          source={{uri: picture[0]}}>
+            <GestureRecognizer 
+              onSwipeLeft={() => getRandomQuote()}>
+              <HomeQuote quote={quote.quote} author={quote.author} />
+            </GestureRecognizer>
         </ImageBackground>
       </View>
+      </GestureRecognizer>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    height: '100%',
-    alignContent: 'center',
-  },
   picture: {
     height: '100%',
     width: '100%',
+    alignContent: 'center',
+    justifyContent: 'center',
   },
 });
--- components/homeQuote.js
+++ components/homeQuote.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 const HomeQuote = ({quote, author}) => {
   return (
     <View style={styles.quoteBox}> 
-      <Text style={styles.quote}>{quote}</Text>
+      <Text style={styles.quote}>" {quote} "</Text>
       <Text style={styles.author}>-{author}</Text>
     </View>
   )
@@ -13,18 +13,22 @@ const HomeQuote = ({quote, author}) => {
 const styles = StyleSheet.create({
   quoteBox: {
     backgroundColor: '#d4d4d4',
+    opacity: 0.7,
     margin: 10,
-    padding: 10,
+    padding: 20,
+    borderRadius: 5,
   },
 
   quote: {
-    fontSize: 16,
+    fontSize: 18,
   },
 
   author: {
     fontSize: 12,
     fontStyle: 'italic',
     textAlign: 'right',
+    marginTop: 5,
+    marginRight: 10,
   }
 })
 
